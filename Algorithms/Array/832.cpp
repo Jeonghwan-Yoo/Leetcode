1b6bb69852b08018454f5f183c4f5195eed6ea60
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-11-10 18:35:14 +0900

    Flipping an Image

---
+++ Algorithms/Array/832.cpp
@@ -0,0 +1,20 @@
+// O(N)
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        int n = (int)A.size();
+        int n2 = (n + 1) >> 1;
+        
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n2; ++j)
+            {
+                int tmp = A[i][j];
+                A[i][j] = A[i][n - j - 1] ^ 1;
+                A[i][n - j - 1] = tmp ^ 1;
+            }
+        }
+        
+        return A;
+    }
+};
