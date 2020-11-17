aa83dc46af858cd5a05a9ae83d2d0c11e18c59ea
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-11-17 17:56:26 +0900

    Mirror Reflection

---
+++ Algorithms/Math/858.cpp
@@ -0,0 +1,20 @@
+// O(log(min(a, b))))
+class Solution {
+public:
+    int Gcd(int p, int q)
+    {
+        if (q == 0)
+            return p;
+        return Gcd(q, p % q);
+    }
+    int mirrorReflection(int p, int q) {
+        int gcd = Gcd(p, q);
+        p /= gcd;
+        p &= 1;
+        q /= gcd;
+        q &= 1;
+        if (p == 1 && q == 1)
+            return 1;
+        return (p == 1 ? 0 : 2);
+    }
+};
