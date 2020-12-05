c1cbaaa5fa92962893f8399aa9b326777b0f0ed4
Jeonghwan Yoo <dwgbjdhks2@gmail.com>
2020-12-06 00:31:49 +0900

    Can Place Flowers

---
+++ Algorithms/Greedy/605.cpp
@@ -0,0 +1,24 @@
+// O(N)
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (n == 0)
+            return true;
+        
+        int len = (int)flowerbed.size();
+        
+        int cnt = 0;
+        for (int i = 0; i < len; ++i)
+        {
+            if ((i == 0 || flowerbed[i - 1] == 0) && flowerbed[i] == 0 && (i == len - 1 || flowerbed[i + 1] == 0))
+            {
+                flowerbed[i] = 1;
+                ++cnt;
+                if (cnt >= n)
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+};
